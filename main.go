3284490d04e2edf2f9dcf088662a223c501bcd5f
a.nurlan <a.nurlan@documentolog.com>
2025-04-03 09:26:17 +0500

    frame of the project an directories

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-  "fmt"
+	"inventory-service/database"
+	"inventory-service/routes"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	r := gin.Default()
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	// Подключение к БД
+	database.ConnectDB()
+
+	// Регистрация маршрутов
+	routes.RegisterRoutes(r)
+
+	// Запуск сервера
+	log.Println("Inventory Service is running on port 8081...")
+	r.Run(":8081")
 }
